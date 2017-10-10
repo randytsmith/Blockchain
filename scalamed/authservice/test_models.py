791517bfe35f88686c2870deb6962e9e1f02e15f
sg <scott.j.greenup@gmail.com>
2017-10-11 08:56:03 +1100

    fix authservice.test_models:*

--- scalamed/authservice/test_models.py
+++ scalamed/authservice/test_models.py
@@ -9,16 +9,18 @@ class UserTestCase(TestCase):
     def setUp(self):
         log.setLevel(10)
 
+        self.user = User.objects.create_user(
+            username=None,
+            email="bob@example.com",
+            password="password123")
+
     def tearDown(self):
         log.setLevel(30)
 
     def test_create_user(self):
         """Test create_user creates a user with working defaults."""
 
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
+        user = self.user
         self.assertFalse(hasattr(user, 'username'))
         self.assertEqual(user.email, "bob@example.com")
         self.assertNotEqual(user.password, "password123")
@@ -26,29 +28,13 @@ class UserTestCase(TestCase):
         self.assertEqual(len(user.uuid), 36)
         self.assertEqual(user.role, User.Role.PATIENT)
 
-    def test_token_level_0_default(self):
+    def test_token_generate(self):
         """Test the generation and validation of tokens"""
+        user = self.user
+        for kind in list(TokenType):
+            token = TokenManager.generate(user, kind)
+            self.assertTrue(TokenManager.validate(user, token, kind))
 
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
-
-        token = user.generate_token(level=0)
-        self.assertTrue(user.validate_token(token, level=0))
-
-    def test_token_level_1_default(self):
-        """Test the generation and validation of tokens"""
-
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
-
-        token = user.generate_token(level=1)
-        self.assertTrue(user.validate_token(token, level=1))
-
-    # TODO patch kind.ttl()
     @mock.patch('authservice.models.TokenType.ttl')
     def test_token_expiry(self, mock_TokenType):
 
@@ -57,34 +43,19 @@ class UserTestCase(TestCase):
 
         mock_TokenType.side_effect = instantly_expire
 
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
-
+        user = self.user
         token = TokenManager.generate(user, TokenType.LEVEL_ZERO)
         claims = TokenManager.validate(user, token, TokenType.LEVEL_ZERO)
         self.assertFalse(claims)
 
-    def test_token_subject(self):
-        pass
-
     def test_counter(self):
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
-
+        user = self.user
         self.assertEquals(user.counter(), 0)
         self.assertEquals(user.counter(), 1)
         self.assertEquals(user.counter(), 2)
         self.assertEquals(user.counter(), 3)
 
-    def test_tokens(self):
-        user = User.objects.create_user(
-            username=None,
-            email="bob@example.com",
-            password="password123")
+    def test_token_generate_other(self):
 
-        t = user.generate_token(level=0)
-        user.validate_token(t, level=0)
+        user = User.objects.
+
